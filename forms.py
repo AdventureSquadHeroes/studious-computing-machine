1176d681eb89ac88b14e5a4e18a9a6856a8094e8
ColbyHand <thehand87@outlook.com>
2023-03-18 16:07:48 -0500

    Added search function and view.html page

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, ValidationError
 
 class SearchForm(FlaskForm):
-    cuisine = StringField("Cuisine Type")
-    ingredients = StringField("Ingredients")
-    equipment = StringField("Type of Equipment used")
+    cuisine = StringField("Search by cuisine type")
+    ingredients = StringField("Search by ingredients")
+    equipment = StringField("Search by type of equipment used")
     submit = SubmitField("Search")
